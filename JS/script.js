2a59010e15bd4b856fc05e92a640309345715c98
juanmagape <juanmagape.trb@gmail.com>
2025-12-09 14:35:32 +0100

    Update - v0.03

--- JS/script.js
+++ JS/script.js
@@ -1,51 +1,84 @@
-const content = document.getElementById("content");
+const navBar = document.getElementById("navbar");
+const myChart = document.getElementById("myChart");
+const contentPage = document.getElementById("content");
+const contentKnow = document.getElementById("contentKnow");
 
-fetch('../JSON/tickets.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log("Tickets cargados:", data);
-    mostrarTickets(data);
-  })
-  .catch(error => console.error("Error cargando JSON:", error));
+let navbarValues = ["Home", "Inbox", "Global", "Knowledge"];
 
-function mostrarTickets(tickets) {
-    content.innerHTML += `  
-    <thead>
-    <tr>
-      <th class="border">ID</th>
-      <th class="border">Asunto</th>
-      <th class="border">Fecha</th>
-      <th>Estado</th>
-    </tr>
-  </thead>`
-    tickets.forEach(ticket => {
-        content.innerHTML += `
-        <tr>
-            <td class="border"><a href="?ticket=${ticket.id}" class="idlink" id">${ticket.id}</a></td>
-            <td class="border">${ticket.asunto}</td>
-            <td class="border">${ticket.fecha_apertura}</td>
-            <td>${ticket.estado}</td>
-        </tr>
-        </div>`;
-    });
+navbarValues.forEach(element => {
+    navBar.innerHTML += `<a href="?link=${element}">${element}</a>`
+});
 
-    const params = new URLSearchParams(window.location.search);
-    const ticketID = params.get("ticket");
-
-    if (ticketID) {
-    fetch('../JSON/tickets.json')
-        .then(res => res.json())
-        .then(tickets => {
-            const ticket = tickets.find(t => t.id === ticketID);
-            const canvas = document.getElementById("myChart");
-
-            canvas.style.display = 'none';
-            
-            content.innerHTML = 
-            `<p>${ticketID}</p>
-            <h3>${ticket.asunto}</h3>
-            <p>${ticket.descripcion}</p>
-            <a href="../index.html">Volver</a>`;
-    }
-    )}
+function cargarpaginas() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentLink = urlParams.get('link');
+    const knowLink = urlParams.get('know'); 
+    
+if (knowLink) {
+    myChart.style.display = 'none';
+    contentPage.style.display = 'none';
+    contentKnow.style.display = 'none'; 
+    
+    mostrarDetalleKnowledge(knowLink);
 }
+else if (currentLink === "Knowledge") { 
+    myChart.style.display = 'none';
+    contentPage.style.display = 'none';
+    
+    fetch('../JSON/knowledges.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("Tickets cargados:", data);
+            mostrarKnowledges(data);
+        })
+        .catch(error => console.error("Error cargando JSON:", error));
+}
+}
+
+function mostrarKnowledges(knowledges) {
+    contentKnow.innerHTML = '';
+    knowledges.forEach(know => {
+        contentKnow.innerHTML += `
+        <div>
+            <h2>${know.nombre}</h2>
+            <p>${know.descripcion_breve}</p>
+            
+            <a href="?know=${know.nombre}">
+                <button class="botonKnow">Ver knowledge</button>
+            </a>
+
+        </div>
+        `;
+    });
+}
+
+function mostrarDetalleKnowledge(linkName) {
+    fetch('../JSON/knowledges.json')
+        .then(res => res.json())
+        .then(knowName => {
+            const knowN = knowName.find(t => t.nombre === linkName);
+            const knowInside = document.getElementById("knowInside");
+            
+            if (knowN) {
+
+                const pasos = knowN.procedimiento
+                .map(paso => `<p class="paso">${paso}</p>`)
+                .join('');
+
+                knowInside.innerHTML = `
+                <div class="knowContent">
+                    <h2>${knowN.nombre}</h2>
+                    <h3>${knowN.descripcion_breve}</h3>
+                    <p class="procedimiento">${pasos}</p>
+                    <a href="?link=Knowledge">
+                        <button class="botonKnow">Volver</button>
+                    </a>
+                </div>
+     
+                `;
+            }
+        })
+        .catch(error => console.error("Error:", error));
+}
+
+cargarpaginas();
---
+++ JS/tickets.js
@@ -0,0 +1,51 @@
+const content = document.getElementById("content");
+
+fetch('../JSON/tickets.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log("Tickets cargados:", data);
+    mostrarTickets(data);
+  })
+  .catch(error => console.error("Error cargando JSON:", error));
+
+function mostrarTickets(tickets) {
+    content.innerHTML += `  
+    <thead>
+    <tr>
+      <th class="border">ID</th>
+      <th class="border">Asunto</th>
+      <th class="border">Fecha</th>
+      <th>Estado</th>
+    </tr>
+  </thead>`
+    tickets.forEach(ticket => {
+        content.innerHTML += `
+        <tr>
+            <td class="border"><a href="?ticket=${ticket.id}" class="idlink" id">${ticket.id}</a></td>
+            <td class="border">${ticket.asunto}</td>
+            <td class="border">${ticket.fecha_apertura}</td>
+            <td>${ticket.estado}</td>
+        </tr>
+        </div>`;
+    });
+
+    const params = new URLSearchParams(window.location.search);
+    const ticketID = params.get("ticket");
+
+    if (ticketID) {
+    fetch('../JSON/tickets.json')
+        .then(res => res.json())
+        .then(tickets => {
+            const ticket = tickets.find(t => t.id === ticketID);
+            const canvas = document.getElementById("myChart");
+
+            canvas.style.display = 'none';
+            
+            content.innerHTML = 
+            `<p>${ticketID}</p>
+            <h3>${ticket.asunto}</h3>
+            <p>${ticket.descripcion}</p>
+            <a href="../index.html">Volver</a>`;
+    }
+    )}
+}
